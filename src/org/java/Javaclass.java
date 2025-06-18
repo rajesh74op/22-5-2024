54e36a8062670ca917536a4b0119f91e24b40dcd
Rajesh <rajeshk7676@gmail.com>
2025-06-18 17:58:10 +0530

    My second

---
+++ src/org/java/Javaclass.java
@@ -0,0 +1,16 @@
+package org.java;
+
+public class Javaclass {
+	public static void main(String[] args) {
+		//3. Print Even count From 1 to 10
+		int evencount =0;
+		for(int i=1; i<=10; i++) {
+			if (i%2==0) {
+				evencount++;
+			
+			}
+	}
+			System.out.println(evencount);
+
+}
+}
